f7b958226191b860864792d49de3c6c6b26f99fa
Benjamin Meichsner <benni@dresdener27.de>
2009-02-02 17:28:59 +0100

    Bugfixing in memberships-filter.

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -2,18 +2,16 @@ class MembershipsController < ApplicationController
     before_filter :authenticate_membership_or_admin
 
   def add_member
-    @group = Group.find(params[:group_id])
     user = User.find(params[:user_id])
     Membership.create(:group => @group, :user => user)
-    redirect_to :action => 'reload', :group_id => @group
+    redirect_to :action => 'reload', :id => @group
   end
 
   def drop_member
     begin
-      group = Group.find(params[:group_id])
       Membership.find(params[:membership_id]).destroy
       if User.find(@current_user.id).role_admin?
-        redirect_to :action => 'reload', :group_id => group
+        redirect_to :action => 'reload', :id => @group
       else
         # If the user drops himself from admin group
         flash[:notice] = MESG_NO_ADMIN_ANYMORE
@@ -21,12 +19,11 @@ class MembershipsController < ApplicationController
       end
     rescue => error
       flash[:error] = error.to_s
-      redirect_to :action => 'reload', :group_id => group
+      redirect_to :action => 'reload', :id => @group
     end
   end
 
   def reload
-    @group = Group.find(params[:group_id])
     render :update do |page|
       page.replace_html 'members', :partial => 'shared/memberships/current_members',  :object => @group
       page.replace_html 'non_members', :partial => 'shared/memberships/non_members', :object => @group
