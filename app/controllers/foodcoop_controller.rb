e32e97d178b1ce292abeaae912b1c2dfcddf31dc
sandoz <sandoz@jpberlin.de>
2009-04-18 15:35:29 +0200

    Added "ordergroups" view to list the ordergroups
    * You can search in the name.
    * You can show only active order groups (order in last 3 months)
    * Future feature: generate text and pdf files for the box
      labels

--- app/controllers/foodcoop_controller.rb
+++ app/controllers/foodcoop_controller.rb
@@ -49,11 +49,19 @@ class FoodcoopController < ApplicationController
       @per_page = 20
     end
 
-    # if somebody uses the search field:
-    conditions = ["name LIKE ?", "%#{params[:query]}%"] unless params[:query].blank?
+    if (params[:only_active].to_i == 1)
+      if (! params[:query].blank?)
+        conditions = ["orders.starts >= ? AND name LIKE ?", Time.now.months_ago(3), "%#{params[:query]}%"]
+      else
+        conditions = ["orders.starts >= ?", Time.now.months_ago(3)]
+      end
+    else
+      # if somebody uses the search field:
+      conditions = ["name LIKE ?", "%#{params[:query]}%"] unless params[:query].blank?
+    end
 
-    @total = Ordergroup.count(:conditions => conditions)
-    @order_groups = Ordergroup.paginate(:page => params[:page], :per_page => @per_page, :conditions => conditions, :order => "name")
+    @total = Ordergroup.count(:conditions => conditions, :include => "orders")
+    @order_groups = Ordergroup.paginate(:page => params[:page], :per_page => @per_page, :conditions => conditions, :order => "name", :include => "orders")
 
     respond_to do |format|
       format.html # index.html.erb
