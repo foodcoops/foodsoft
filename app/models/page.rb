0d11e68bad5ea7ff891dd1a2f1475c0fd4bd68b6
Benjamin Meichsner <benni@dresdener27.de>
2009-05-13 17:32:43 +0200

    Added syntax help to wiki form.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,11 +1,12 @@
 class Page < ActiveRecord::Base
 
   validates_presence_of :title, :body
+  validates_uniqueness_of :permalink
 
-  before_save :set_permalink
+  before_validation_on_create :set_permalink
 
   def set_permalink
-    if self.permalink.blank? #FIXME: or title.changed?
+    if self.permalink.blank?
       self.permalink = Page.count == 0 ? "home" : "#{title.downcase.strip.gsub(/ |\.|@/, '-')}"
     end
   end
