2660d86b89b1fcb953e639d40d4a18e1bcd8b5ff
Benjamin Meichsner <benni@dresdener27.de>
2009-05-20 14:06:40 +0200

    Added table of contents to wiki page.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,7 +1,7 @@
 module PagesHelper
 
   def wikified_body(body)
-    r = RedCloth.new(body)
+    r = RedCloth.new(generate_anchors(body))
     r.gsub!(/\[\[(.*?)(\|(.*?))?\]\]/) { wiki_link($1, $3) }
     sanitize r.to_html
     r.to_html
@@ -15,4 +15,25 @@ module PagesHelper
       link_to((link_text || wiki_words), wiki_page_url(permalink), :class => "new_wiki_link")
     end
   end
+
+  def generate_toc(body)
+    toc = ""
+    body.gsub(/^\s*h([1-6])\.\s+(.*)/) do
+      number = $1
+      name = $2
+      header = name.downcase.gsub(' ', '-')
+      toc << '#' * number.to_i + ' "' + name + '":#' + header + "\n"
+    end
+    RedCloth.new(toc).to_html
+  end
+
+  def generate_anchors(body)
+    body.gsub(/^\s*h([1-6])\.\s+(.*)/) do
+      number = $1
+      name = $2
+      header = name.downcase.gsub(' ', '-')
+      "\nh#{number}. #{name}<a name ='#{header}'> </a>"
+    end
+  end
+
 end
