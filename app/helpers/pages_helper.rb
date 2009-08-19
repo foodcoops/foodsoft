004ce464c132c6469c0b91590d95b2b009679597
sandoz <sandoz@jpberlin.de>
2009-08-19 22:59:53 +0200

    Fix problem in breadcrumps, when no "Home"-page exists.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -20,6 +20,22 @@ module PagesHelper
       link_to text, "/wiki/#{page.title}"
     end
   end
+
+
+  def link_to_wikipage_by_permalink(permalink, text = nil)
+    unless permalink.blank?
+      page = Page.find_by_permalink(permalink)
+      if page.nil?
+        if text.nil?
+          link_to permalink,  new_page_path(:title => permalink)
+        else
+          link_to text,  new_page_path(:title => permalink)
+        end
+      else
+        link_to_wikipage(page, text)
+      end
+    end
+  end
 #  def generate_toc(body)
 #    toc = ""
 #    body.gsub(/^([=]{1,6})\s*(.*?)\s*(\1)/) do
