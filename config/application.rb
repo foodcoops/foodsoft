344abf5a265ff653444c3926b68c6750ca175dae
benni <benni@posteo.de>
2012-12-22 18:38:49 +0100

    Deployment stuff. Ignore capistrano. Faster precompiling.

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Foodsoft
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Do not enable database connection when precompiling assets
+    config.assets.initialize_on_precompile = false
   end
 end
