e06524ca37d7668815d73acd863442eda00a7811
wvengen <dev-voko@willem.engen.nl>
2013-02-08 01:52:20 +0100

    finish ordergroups and orders i18n + controller

--- app/models/ordergroup.rb
+++ app/models/ordergroup.rb
@@ -15,7 +15,7 @@ class Ordergroup < Group
   has_many :group_orders
   has_many :orders, :through => :group_orders
 
-  validates_numericality_of :account_balance, :message => 'ist keine gültige Zahl'
+  validates_numericality_of :account_balance, :message => I18n.t('ordergroups.model.invalid_balance')
   validate :uniqueness_of_members
 
   after_create :update_stats!
@@ -103,7 +103,7 @@ class Ordergroup < Group
   # Make sure, that a user can only be in one ordergroup
   def uniqueness_of_members
     users.each do |user|
-      errors.add :user_tokens, "#{user.nick} ist schon in einer anderen Bestellgruppe" if user.groups.where(:type => 'Ordergroup').size > 1
+      errors.add :user_tokens, I18n.t('ordergroups.model.error_single_group', :user => user.nick) if user.groups.where(:type => 'Ordergroup').size > 1
     end
   end
   
