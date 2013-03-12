6196bdae410871e4b86762f8b2ee19c410ceb6b5
Benjamin Meichsner <benni@posteo.de>
2013-03-12 12:34:05 +0100

    Added some eager loading for balancing new.

--- app/controllers/finance/balancing_controller.rb
+++ app/controllers/finance/balancing_controller.rb
@@ -21,8 +21,10 @@ class Finance::BalancingController < Finance::BaseController
       sort = "id"
     end
 
-    @articles = @order.order_articles.ordered.includes(:article).order(sort)
-      
+    @articles = @order.order_articles.ordered.includes(:order, :article, :article_price,
+                                                       group_order_articles: {group_order: :ordergroup}).order(sort)
+
+
     if params[:sort] == "order_number"
       @articles = @articles.to_a.sort { |a,b| a.article.order_number.gsub(/[^[:digit:]]/, "").to_i <=> b.article.order_number.gsub(/[^[:digit:]]/, "").to_i }
     elsif params[:sort] == "order_number_reverse"
