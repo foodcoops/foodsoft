79d1c5a66ddce24291732e4fb2002be421ad5b94
Benjamin Meichsner <benni@posteo.de>
2013-04-08 14:40:02 +0200

    Allow article price equal to zero. Closes #111

--- app/models/article.rb
+++ app/models/article.rb
@@ -18,7 +18,8 @@ class Article < ActiveRecord::Base
   validates_presence_of :name, :unit, :price, :tax, :deposit, :unit_quantity, :supplier_id, :article_category
   validates_length_of :name, :in => 4..60
   validates_length_of :unit, :in => 2..15
-  validates_numericality_of :price, :unit_quantity, :greater_than => 0
+  validates_numericality_of :price, :greater_than_or_equal_to => 0
+  validates_numericality_of :unit_quantity, :greater_than => 0
   validates_numericality_of :deposit, :tax
   validates_uniqueness_of :name, :scope => [:supplier_id, :deleted_at, :type]
   
