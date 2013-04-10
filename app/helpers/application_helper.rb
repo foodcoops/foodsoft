6c6579902597881dd64ca8df67b54b8ca9644b5d
wvengen <dev-voko@willem.engen.nl>
2013-04-10 17:34:24 +0200

    use more beautiful close mark

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -152,7 +152,7 @@ module ApplicationHelper
       type = :success if type == :notice
       type = :error   if type == :alert
       text = content_tag(:div,
-                         content_tag(:button, I18n.t('ui.marks.close'), :class => "close", "data-dismiss" => "alert") +
+                         content_tag(:button, I18n.t('ui.marks.close').html_safe, :class => "close", "data-dismiss" => "alert") +
                              message, :class => "alert fade in alert-#{type}")
       flash_messages << text if message
     end
