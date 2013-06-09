7c4ee01ff2c6b5195a1bdb7b72cc87f4799a8071
Benjamin Meichsner <benni@posteo.de>
2013-06-09 18:02:47 +0200

    Define autofill attributes when creating new supplier from shared_supplier.

--- app/models/shared_supplier.rb
+++ app/models/shared_supplier.rb
@@ -7,6 +7,12 @@ class SharedSupplier < ActiveRecord::Base
 
   has_one :supplier
   has_many :shared_articles, :foreign_key => :supplier_id
-  
+
+  # These set of attributes are used to autofill attributes of new supplier,
+  # when created by import from shared supplier feature.
+  def autofill_attributes
+    whitelist = %w(name address phone fax email url delivery_days note)
+    attributes.select { |k,_v| whitelist.include?(k) }
+  end
 end
 
