dd670cdfee4b30d9e5dc22219da2453f836b2bdc
wvengen <dev-voko@willem.engen.nl>
2013-06-26 11:34:14 +0200

    allow sorting on available funds as too
    Conflicts:
    	app/views/finance/ordergroups/_ordergroups.html.haml

--- app/controllers/finance/ordergroups_controller.rb
+++ app/controllers/finance/ordergroups_controller.rb
@@ -7,6 +7,8 @@ class Finance::OrdergroupsController < Finance::BaseController
                when "account_balance" then "account_balance"
                when "name_reverse" then "name DESC"
                when "account_balance_reverse" then "account_balance DESC"
+               when "available_funds" then "available_funds"
+               when "available_funds_reverse" then "available_funds DESC"
              end
     else
       sort = "name"
@@ -14,6 +16,7 @@ class Finance::OrdergroupsController < Finance::BaseController
 
     @ordergroups = Ordergroup.undeleted.order(sort)
     @ordergroups = @ordergroups.where('name LIKE ?', "%#{params[:query]}%") unless params[:query].nil?
+    @ordergroups = @ordergroups.select("*, (SELECT COALESCE(SUM(price),0) FROM group_orders INNER JOIN orders WHERE group_orders.order_id=orders.id AND group_orders.ordergroup_id=groups.id AND orders.state IN ('open', 'finished')) AS available_funds")
 
     @ordergroups = @ordergroups.page(params[:page]).per(@per_page)
   end
