7c27017306630f72166244279f9b8115a5575305
Julius <JuliusRapp@gmx.de>
2013-07-11 18:37:55 +0200

    Link delivery/stock_taking/order in stock_changes index

--- app/helpers/stock_changes_helper.rb
+++ app/helpers/stock_changes_helper.rb
@@ -2,11 +2,11 @@ module StockChangesHelper
 
   def link_to_stock_change_reason(stock_change)
     if stock_change.delivery_id
-      t '.delivery'
+      link_to t('.delivery'), supplier_delivery_path(stock_change.delivery.supplier, stock_change.delivery)
     elsif stock_change.order_id
-      t '.order'
+      link_to t('.order'), order_path(stock_change.order)
     elsif stock_change.stock_taking_id
-      t '.stock_taking'
+      link_to t('.stock_taking'), stock_taking_path(stock_change.stock_taking)
     end
   end
 
