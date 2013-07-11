ffcb9490c96de025cc247accd1218dba9b5e9c4e
Julius <JuliusRapp@gmx.de>
2013-07-11 18:20:24 +0200

    Make stock_changes readonly in index view

--- app/controllers/stock_changes_controller.rb
+++ app/controllers/stock_changes_controller.rb
@@ -3,6 +3,6 @@ class StockChangesController < ApplicationController
   before_filter :find_stock_article
   
   def index
-    @stock_changes = @stock_article.stock_changes(:readonly => true).order('stock_changes.created_at DESC') # The readonly has no effect, what is the proper way to achieve that?
+    @stock_changes = @stock_article.stock_changes.order('stock_changes.created_at DESC').each {|s| s.readonly!}
   end
 end
