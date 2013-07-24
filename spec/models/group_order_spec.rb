c753ae04715fe577559dd6530f7601f07a8fde78
wvengen <dev-voko@willem.engen.nl>
2013-07-25 01:07:41 +0200

    comment tests that are unclear how they should behave

--- spec/models/group_order_spec.rb
+++ spec/models/group_order_spec.rb
@@ -5,13 +5,15 @@ describe GroupOrder do
   let(:supplier) { FactoryGirl.create :supplier, article_count: true }
   let(:order) { FactoryGirl.create(:order, supplier: supplier, article_ids: supplier.articles.map(&:id)).reload }
 
-  it 'needs an order' do
-    expect(FactoryGirl.build(:group_order, ordergroup: user.ordergroup)).to be_invalid
-  end
+  # the following two tests are currently disabled - https://github.com/foodcoops/foodsoft/issues/158
 
-  it 'needs an ordergroup' do
-    expect(FactoryGirl.build(:group_order, order: order)).to be_invalid
-  end
+  #it 'needs an order' do
+  #  expect(FactoryGirl.build(:group_order, ordergroup: user.ordergroup)).to be_invalid
+  #end
+
+  #it 'needs an ordergroup' do
+  #  expect(FactoryGirl.build(:group_order, order: order)).to be_invalid
+  #end
 
   describe do
     let(:go) { FactoryGirl.create :group_order, order: order, ordergroup: user.ordergroup }
