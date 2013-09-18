f462e70e495b15fa7d731452573f913a4efae6f5
Manuel Wiedenmann <manuel@funkensturm.de>
2013-09-18 12:44:41 +0200

    uses FactoryGirl helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
-require 'support/coverage' # needs to be first
+require_relative 'support/coverage' # needs to be first
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
