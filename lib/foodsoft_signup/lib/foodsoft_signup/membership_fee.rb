b623a879f1dff4a6ce08ad343b1a3a726e84a0d6
wvengen <dev-voko@willem.engen.nl>
2013-11-06 00:13:16 +0100

    approve membership when membership fee is payed

--- lib/foodsoft_signup/lib/foodsoft_signup/membership_fee.rb
+++ lib/foodsoft_signup/lib/foodsoft_signup/membership_fee.rb
@@ -5,8 +5,9 @@ module FoodsoftSignup
   module DebitMembership
     def self.included(base) # :nodoc:
       base.class_eval do
-        after_create :debit_membership!, :unless => Proc.new { FoodsoftConfig[:membership_fee].nil? }
 
+        # debit membership on ordergroup creation
+        after_create :debit_membership!, :unless => Proc.new { FoodsoftConfig[:membership_fee].nil? }
         def debit_membership!
           amount = (-FoodsoftConfig[:membership_fee]).to_s
           amount.gsub!('\.', I18n.t('separator', :scope => 'number.format')) # workaround localize_input problem
@@ -19,6 +20,18 @@ module FoodsoftSignup
             save!
           end
         end
+
+        # approve ordergroup when membership fee is payed
+        alias_method :foodsoft_signup_orig_add_financial_transaction!, :add_financial_transaction!
+        def add_financial_transaction!(amount, note, user)
+          result = self.foodsoft_signup_orig_add_financial_transaction!(amount, note, user)
+          if not self.approved? and amount >= (FoodsoftConfig[:membership_fee] - 1e-3)
+            self.approved = true
+            save!
+          end
+          result
+        end
+
       end
     end
   end
