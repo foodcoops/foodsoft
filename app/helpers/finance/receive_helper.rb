2d991412295c77156adabbae7822708499f8d99d
wvengen <dev-voko@willem.engen.nl>
2013-11-26 13:31:07 +0100

    only allow to add articles not already present in receive

--- app/helpers/finance/receive_helper.rb
+++ app/helpers/finance/receive_helper.rb
@@ -1,8 +1,11 @@
 # :encoding:utf-8:
 module Finance::ReceiveHelper
   # TODO currently duplicate a bit of DeliveriesHelper.articles_for_select2
-  def articles_for_select2(supplier)
-    supplier.articles.undeleted.reorder('articles.name ASC').map do |a|
+  # except is an array of article id's to omit
+  def articles_for_select2(supplier, except = [])
+    articles = supplier.articles.reorder('articles.name ASC')
+    articles.reject! {|a| not except.index(a.id).nil? } if except
+    articles.map do |a|
       {:id => a.id, :text => "#{a.name} (#{a.unit_quantity}⨯#{a.unit})"}
     end
   end
