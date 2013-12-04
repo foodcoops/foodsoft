bdec572a52cbac089fa5935e3e4d62a228e93ad3
wvengen <dev-voko@willem.engen.nl>
2013-12-04 18:47:31 +0100

    fix article title i18n

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -17,6 +17,14 @@ module ArticlesHelper
     Hash[params[:search].map { |k,v| [k, (v.is_a?(Array) ? v.join(" ") : v)] }]
   end
 
+  # title attribute with extra article information
+  def article_info_title(article)
+    order_title = []
+    order_title << Article.human_attribute_name(:manufacturer)+': ' + article.manufacturer unless article.manufacturer.to_s.empty?
+    order_title << Article.human_attribute_name(:note)+': ' + article.note unless article.note.to_s.empty?
+    order_title.join("\n")
+  end
+
   # show icon with link to product information when available
   def article_info_icon(article)
     icon = "<i class='icon-info-sign'></i>".html_safe
