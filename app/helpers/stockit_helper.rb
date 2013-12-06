111c061efa533115e9204356f87d7b6098995439
wvengen <dev-voko@willem.engen.nl>
2013-12-06 22:53:59 +0100

    more i18n cleanup (affects foodcoops#137)

--- app/helpers/stockit_helper.rb
+++ app/helpers/stockit_helper.rb
@@ -7,11 +7,11 @@ module StockitHelper
 
   def link_to_stock_change_reason(stock_change)
     if stock_change.delivery_id
-      link_to t('.delivery'), supplier_delivery_path(stock_change.delivery.supplier, stock_change.delivery)
+      link_to Delivery.model_name.human, supplier_delivery_path(stock_change.delivery.supplier, stock_change.delivery)
     elsif stock_change.order_id
-      link_to t('.order'), order_path(stock_change.order)
+      link_to Order.model_name.human, order_path(stock_change.order)
     elsif stock_change.stock_taking_id
-      link_to t('.stock_taking'), stock_taking_path(stock_change.stock_taking)
+      link_to StockTaking.model_name.human, stock_taking_path(stock_change.stock_taking)
     end
   end
 end
