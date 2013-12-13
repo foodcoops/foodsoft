9b27ce29e9c4cf837266d56ec8f0d85c21b7c2d6
wvengen <dev-voko@willem.engen.nl>
2013-12-14 00:46:13 +0100

    fix BigDecimal to_s monkey-patch (complements 0052d4af1a4cc397c4248a7049a52e35ce33cec1)

--- config/initializers/number_display.rb
+++ config/initializers/number_display.rb
@@ -10,8 +10,8 @@ end
 if defined? BigDecimal
   class BigDecimal
     alias :foodsoft_to_s :to_s
-    def to_s
-      foodsoft_to_s.chomp(".0")
+    def to_s(format = DEFAULT_STRING_FORMAT)
+      foodsoft_to_s(format).chomp(".0")
     end
   end
 end
