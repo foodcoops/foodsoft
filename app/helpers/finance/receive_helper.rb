1ab09b41bde0593afadca8d6e4309eb3f6f62e76
wvengen <dev-voko@willem.engen.nl>
2013-12-18 17:34:45 +0100

    fix articles_for_select2 helper

--- app/helpers/finance/receive_helper.rb
+++ app/helpers/finance/receive_helper.rb
@@ -1,12 +1,3 @@
 # :encoding:utf-8:
 module Finance::ReceiveHelper
-  # TODO currently duplicate a bit of DeliveriesHelper.articles_for_select2
-  # except is an array of article id's to omit
-  def articles_for_select2(supplier, except = [])
-    articles = supplier.articles.reorder('articles.name ASC')
-    articles.reject! {|a| not except.index(a.id).nil? } if except
-    articles.map do |a|
-      {:id => a.id, :text => "#{a.name} (#{a.unit_quantity}⨯#{a.unit})"}
-    end
-  end
 end
