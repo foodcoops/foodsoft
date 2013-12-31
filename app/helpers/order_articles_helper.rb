5349ee142eba8a85837046582e8ad2712a7498d9
Julius <JuliusRapp@gmx.de>
2013-12-31 12:35:30 +0100

    Offer _undeleted_ StockArticles for new OrderArticle only

--- app/helpers/order_articles_helper.rb
+++ app/helpers/order_articles_helper.rb
@@ -2,9 +2,9 @@ module OrderArticlesHelper
 
   def new_order_articles_collection
     if @order.stockit?
-      StockArticle.order('articles.name')
+      StockArticle.undeleted.reorder('articles.name')
     else
-      @order.supplier.articles.undeleted.order('articles.name')
+      @order.supplier.articles.undeleted.reorder('articles.name')
     end
   end
 end
