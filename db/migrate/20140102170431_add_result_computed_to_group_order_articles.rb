ebe2966895ca718fc949da2c23fe2d8d3b9fef01
wvengen <dev-voko@willem.engen.nl>
2014-01-03 12:50:49 +0100

    fix migration, complements 7aae7f4d5564cdf6e7e7bdd51657b560ad8f8b8e

--- db/migrate/20140102170431_add_result_computed_to_group_order_articles.rb
+++ db/migrate/20140102170431_add_result_computed_to_group_order_articles.rb
@@ -1,7 +1,6 @@
 class AddResultComputedToGroupOrderArticles < ActiveRecord::Migration
   def change
     add_column :group_order_articles, :result_computed,
-      :decimal, :precision => 8, :scale => 3,
-      :null => false, :default => 0
+      :decimal, :precision => 8, :scale => 3
   end
 end
