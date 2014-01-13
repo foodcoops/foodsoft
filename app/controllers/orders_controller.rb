bab28d27706308b14bda1768f60ae6ae22565c8d
wvengen <dev-voko@willem.engen.nl>
2014-01-13 23:21:03 +0100

    i18n update

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,7 +9,7 @@ class OrdersController < ApplicationController
   # List orders
   def index
     @open_orders = Order.open.includes(:supplier)
-    @orders_in_progress = Order.finished_not_closed.includes(:supplier)
+    @finished_orders = Order.finished_not_closed.includes(:supplier)
     @per_page = 15
     if params['sort']
       sort = case params['sort']
