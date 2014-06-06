ffd1c067292ad504ee309f555c99077923be4b74
wvengen <dev-voko@willem.engen.nl>
2014-06-06 12:45:21 +0200

    fix number crash

--- lib/order_pdf.rb
+++ lib/order_pdf.rb
@@ -35,7 +35,7 @@ class OrderPdf < Prawn::Document
 
   # XXX avoid underscore instead of unicode whitespace in pdf :/
   def number_to_currency(number, options={})
-    super(number, options).gsub("\u202f", ' ')
+    super(number, options).gsub("\u202f", ' ') if number
   end
 
   # return fontsize after scaling it with any configured factor
