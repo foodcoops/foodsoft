cdc746b3640ad2a51125d1a8c38913b0966d2349
wvengen <dev-voko@willem.engen.nl>
2014-06-21 14:28:22 +0200

    update production environment to match 3.3.0 version

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Foodsoft::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
@@ -74,6 +74,9 @@ Foodsoft::Application.configure do
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
+
+  # Use sendmail to avoid ssl cert problems
+  config.action_mailer.delivery_method = :sendmail
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
