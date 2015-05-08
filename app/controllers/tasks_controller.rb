a00e7c94bece8d85da2c786f40a7196517f3f5d0
wvengen <dev-voko@willem.engen.nl>
2015-05-08 17:39:20 +0200

    Rework user-interface for editing recurring tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,21 +1,21 @@
 # encoding: utf-8
 class TasksController < ApplicationController
   #auto_complete_for :user, :nick
-  
+
   def index
     @non_group_tasks = Task.non_group.includes(assignments: :user)
     @groups = Workgroup.includes(open_tasks: {assignments: :user})
   end
-  
+
   def user
     @unaccepted_tasks = Task.unaccepted_tasks_for(current_user)
     @accepted_tasks = Task.accepted_tasks_for(current_user)
   end
-  
+
   def new
     @task = Task.new(current_user_id: current_user.id)
   end
-  
+
   def create
     @task = Task.new(params[:task])
     if params[:periodic]
@@ -27,19 +27,17 @@ class TasksController < ApplicationController
       render :template => "tasks/new"
     end
   end
-  
+
   def show
     @task = Task.find(params[:id])
   end
-  
+
   def edit
     @task = Task.find(params[:id])
+    @periodic = !!params[:periodic]
     @task.current_user_id = current_user.id
-    if @task.periodic?
-      flash.now[:alert] = I18n.t('tasks.edit.warning_periodic').html_safe
-    end
   end
-  
+
   def update
     @task = Task.find(params[:id])
     task_group = @task.periodic_task_group
@@ -61,7 +59,7 @@ class TasksController < ApplicationController
       render :template => "tasks/edit"
     end
   end
-  
+
   def destroy
     task = Task.find(params[:id])
     # Save user_ids to update apple statistics after destroy
@@ -76,7 +74,7 @@ class TasksController < ApplicationController
 
     redirect_to tasks_url, :notice => I18n.t('tasks.destroy.notice')
   end
-  
+
   # assign current_user to the task and set the assignment to "accepted"
   # if there is already an assignment, only accepted will be set to true
   def accept
@@ -88,23 +86,23 @@ class TasksController < ApplicationController
     end
     redirect_to user_tasks_path, :notice => I18n.t('tasks.accept.notice')
   end
-  
+
   # deletes assignment between current_user and given task
   def reject
     Task.find(params[:id]).users.delete(current_user)
     redirect_to :action => "index"
   end
-  
+
   def set_done
     Task.find(params[:id]).update_attribute :done, true
     redirect_to tasks_url, :notice => I18n.t('tasks.set_done.notice')
   end
-  
+
   # Shows all tasks, which are already done
   def archive
     @tasks = Task.done.page(params[:page]).per(@per_page).order('tasks.updated_on DESC').includes(assignments: :user)
   end
-  
+
   # shows workgroup (normal group) to edit weekly_tasks_template
   def workgroup
     @group = Group.find(params[:workgroup_id])
