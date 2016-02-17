01cf6431a97f6a159fdd44fcdf65619e49a3fc9b
Patrick Gansterer <paroga@paroga.com>
2016-02-17 12:26:21 +0100

    Add messagegroup
    Messagegroups are a new kind of group. Every user can join and leave such a
    group on her own. They are useful to create topics for mails.
    
    E.g. if there is a weekly newsletter we can create a new messagegroup for
    it and every user which is interested in receiving the newsletter can join
    the corresponding group.

---
+++ plugins/messages/app/controllers/messagegroups_controller.rb
@@ -0,0 +1,18 @@
+class MessagegroupsController < ApplicationController
+
+  def index
+    @messagegroups = Messagegroup.order("name")
+  end
+
+  def join
+    @messagegroup = Messagegroup.find(params[:id])
+    @messagegroup.users << current_user
+    redirect_to messagegroups_url, :notice => I18n.t('messagegroups.join.notice')
+  end
+
+  def leave
+    @messagegroup = Messagegroup.find(params[:id])
+    @messagegroup.users.destroy(current_user)
+    redirect_to messagegroups_url, :notice => I18n.t('messagegroups.leave.notice')
+  end
+end
