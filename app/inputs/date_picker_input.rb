42eb6c8509b97d030274c8caa9950ae871edc6af
wvengen <dev-voko@willem.engen.nl>
2016-03-11 13:56:02 +0100

    Make ordergroup break fields a bit nicer

--- app/inputs/date_picker_input.rb
+++ app/inputs/date_picker_input.rb
@@ -1,6 +1,6 @@
 class DatePickerInput < SimpleForm::Inputs::StringInput
   def input(wrapper_options)
     options = merge_wrapper_options(input_html_options, wrapper_options)
-    @builder.text_field attribute_name, options.merge(class: 'datepicker')
+    @builder.text_field attribute_name, options.merge(class: 'input-small datepicker')
   end
 end
