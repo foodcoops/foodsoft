9e0ec35e9c7c53ba6cbbbd85d2794e5196822e51
wvengen <dev-voko@willem.engen.nl>
2016-03-30 20:27:00 +0200

    Fix schema version (closes foodcoops/foodsoft#425)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160224201529) do
+ActiveRecord::Schema.define(version: 20160226000000) do
 
   create_table "article_categories", force: :cascade do |t|
     t.string "name",        limit: 255, default: "", null: false
