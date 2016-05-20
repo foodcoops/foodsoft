385c7a21a78c434be1369f021dfcca4708d7f778
wvengen <dev-voko@willem.engen.nl>
2016-05-20 22:28:13 +0200

    Allow querying ordered order_articles in API

--- app/controllers/api/v1/order_articles_controller.rb
+++ app/controllers/api/v1/order_articles_controller.rb
@@ -16,4 +16,22 @@ class Api::V1::OrderArticlesController < Api::V1::BaseController
   def scope
     OrderArticle.joins(:order).merge(Order.open).includes(:article, :article_price)
   end
+
+  def search_scope
+    merge_ordered_scope(super, params.fetch(:q, {})[:ordered])
+  end
+
+  def merge_ordered_scope(scope, ordered)
+    if ordered.blank?
+      scope
+    elsif ordered == 'member'
+      scope.joins(:group_order_articles).merge(current_ordergroup.group_order_articles)
+    elsif ordered == 'all'
+      scope.where('quantity > 0 OR tolerance > 0')
+    elsif ordered == 'supplier'
+      scope.ordered
+    else
+      scope.none # as a hint that it's an invalid value
+    end
+  end
 end
