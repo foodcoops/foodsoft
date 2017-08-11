10fd4626216522d524b9364652c33d49247e12f0
Patrick Gansterer <paroga@paroga.com>
2017-08-11 03:57:39 +0200

    Show only undeleted users in user select input fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   # Currently used to display users nick and ids for autocomplete
   def index
-    @users = User.natural_search(params[:q])
+    @users = User.undeleted.natural_search(params[:q])
     respond_to do |format|
       format.json { render :json => @users.map(&:token_attributes) }
     end
