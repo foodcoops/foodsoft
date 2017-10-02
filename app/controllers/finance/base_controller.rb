8fe633fc424136375b3a3c83f98a5b0d2f10440d
Patrick Gansterer <paroga@paroga.com>
2017-10-02 16:05:18 +0200

    Include nested objects in database queries of Finance::BaseController

--- app/controllers/finance/base_controller.rb
+++ app/controllers/finance/base_controller.rb
@@ -2,9 +2,9 @@ class Finance::BaseController < ApplicationController
   before_filter :authenticate_finance
 
   def index
-    @financial_transactions = FinancialTransaction.order('created_on DESC').limit(8)
-    @orders = Order.finished_not_closed
-    @unpaid_invoices = Invoice.unpaid
+    @financial_transactions = FinancialTransaction.includes(:ordergroup).order('created_on DESC').limit(8)
+    @orders = Order.finished_not_closed.includes(:supplier)
+    @unpaid_invoices = Invoice.unpaid.includes(:supplier)
 
     render template: 'finance/index'
   end
