386bc7382d479bb6801411c9d145b9ea389552fc
Patrick Gansterer <paroga@paroga.com>
2017-10-08 12:33:44 +0200

    Support integers for price_markup
    The config panel writes an integer into the settings table in the
    database, which results in a wrong calculation of the fc_price.

--- app/models/concerns/price_calculation.rb
+++ app/models/concerns/price_calculation.rb
@@ -4,11 +4,17 @@ module PriceCalculation
   # Gross price = net price + deposit + tax.
   # @return [Number] Gross price.
   def gross_price
-    ((price + deposit) * (tax / 100 + 1)).round(2)
+    add_percent(price + deposit, tax)
   end
 
   # @return [Number] Price for the foodcoop-member.
   def fc_price
-    (gross_price  * (FoodsoftConfig[:price_markup] / 100 + 1)).round(2)
+    add_percent(gross_price, FoodsoftConfig[:price_markup])
+  end
+
+  private
+
+  def add_percent(value, percent)
+    (value * (percent * 0.01 + 1)).round(2)
   end
 end
