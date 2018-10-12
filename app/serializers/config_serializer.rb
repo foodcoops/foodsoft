235e3c45836033d35c140e769b6401924c6b1da4
wvengen <dev-voko@willem.engen.nl>
2018-10-12 22:38:34 +0200

    Add footer to API

--- app/serializers/config_serializer.rb
+++ app/serializers/config_serializer.rb
@@ -2,7 +2,7 @@ class ConfigSerializer < ActiveModel::Serializer
   attributes :name, :homepage, :contact,
              :price_markup, :default_locale, :currency_unit, :currency_space,
              :use_tolerance, :tolerance_is_costly, :use_apple_points,
-             :help_url, :applepear_url, :foodsoft
+             :help_url, :applepear_url, :page_footer_html, :foodsoft
 
   def foodsoft
     {
@@ -10,5 +10,16 @@ class ConfigSerializer < ActiveModel::Serializer
       revision: Foodsoft::REVISION,
       url: object[:foodsoft_url]
     }
+  end
+
+  def page_footer_html
+    # also see footer layout
+    if FoodsoftConfig[:page_footer].present?
+      FoodsoftConfig[:page_footer]
+    elsif FoodsoftConfig[:homepage].present?
+      ActionController::Base.helpers.link_to(FoodsoftConfig[:name], FoodsoftConfig[:homepage])
+    else
+      FoodsoftConfig[:name]
+    end
   end
 end
