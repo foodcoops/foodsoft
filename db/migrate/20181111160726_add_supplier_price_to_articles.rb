6d078a00ff55c8799d5276b3fe7aae719ea7f785
Tom Carchrae <tom@intellecti.ca>
2018-11-13 05:15:03 -0800

    replace rocket => with json :

--- db/migrate/20181111160726_add_supplier_price_to_articles.rb
+++ db/migrate/20181111160726_add_supplier_price_to_articles.rb
@@ -1,7 +1,5 @@
 class AddSupplierPriceToArticles < ActiveRecord::Migration
   def change
-    unless column_exists? :articles, :supplier_price
-      add_column :articles, :supplier_price, :decimal, precision: 8, scale: 2
-    end
+    add_column :articles, :supplier_price, :decimal, precision: 8, scale: 2
   end
 end
--- db/migrate/20181111160913_add_supplier_price_to_article_prices.rb
+++ db/migrate/20181111160913_add_supplier_price_to_article_prices.rb
@@ -1,8 +1,5 @@
 class AddSupplierPriceToArticlePrices < ActiveRecord::Migration
   def change
-    unless column_exists? :article_prices, :supplier_price
-        add_column :article_prices, :supplier_price, :decimal, precision: 8, scale: 2
-    end
-
+    add_column :article_prices, :supplier_price, :decimal, precision: 8, scale: 2
   end
 end
