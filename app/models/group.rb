3c6547592bc4b9446ed95d06b00a0e2b8b1942c0
Patrick Gansterer <paroga@paroga.com>
2018-12-18 15:48:27 +0100

    Do not show deleted users as members of groups (fixes #587)

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,7 +6,7 @@ class Group < ActiveRecord::Base
   include MarkAsDeletedWithName
 
   has_many :memberships, dependent: :destroy
-  has_many :users, :through => :memberships
+  has_many :users, -> { where(deleted_at: nil) }, through: :memberships
 
   validates :name, :presence => true, :length => {:in => 1..25}
 
