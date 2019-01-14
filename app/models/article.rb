53096889a7f04148389d1510b89600336703d0f4
Tom Carchrae <tom@intellecti.ca>
2019-01-14 13:01:12 -0800

    unique name/unit check doesn't consider manufacture+origin

--- app/models/article.rb
+++ app/models/article.rb
@@ -236,7 +236,9 @@ class Article < ApplicationRecord
     if supplier && (supplier.shared_sync_method.blank? || supplier.shared_sync_method == 'import')
       errors.add :name, :taken if matches.any?
     else
-      errors.add :name, :taken_with_unit if matches.where(unit: unit, unit_quantity: unit_quantity).any?
+      if matches.where(unit: unit, unit_quantity: unit_quantity, manufacturer: manufacturer, origin: origin).any?
+        errors.add :name, :taken_with_unit
+      end
     end
   end
 
