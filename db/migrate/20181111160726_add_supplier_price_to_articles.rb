dea81ad6aa7b77289a235acf6f13484359114951
Tom Carchrae <tom@intellecti.ca>
2019-03-03 17:06:03 -0800

    my branch had these before this migration - not to be committed to main

--- db/migrate/20181111160726_add_supplier_price_to_articles.rb
+++ db/migrate/20181111160726_add_supplier_price_to_articles.rb
@@ -1,5 +1,7 @@
 class AddSupplierPriceToArticles < ActiveRecord::Migration
   def change
-    add_column :articles, :supplier_price, :decimal, precision: 8, scale: 2
+    unless column_exists? :articles, :supplier_price
+      add_column :articles, :supplier_price, :decimal, precision: 8, scale: 2
+    end
   end
 end
--- db/migrate/20181111160913_add_supplier_price_to_article_prices.rb
+++ db/migrate/20181111160913_add_supplier_price_to_article_prices.rb
@@ -1,5 +1,8 @@
 class AddSupplierPriceToArticlePrices < ActiveRecord::Migration
   def change
-    add_column :article_prices, :supplier_price, :decimal, precision: 8, scale: 2
+    unless column_exists? :article_prices, :supplier_price
+        add_column :article_prices, :supplier_price, :decimal, precision: 8, scale: 2
+    end
+
   end
 end
