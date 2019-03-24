b551389e0c4ec836d6ee4df457955ef72a3941e0
Tom Carchrae <tom@intellecti.ca>
2019-03-24 08:48:13 -0700

    use a sensible default if no redis.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,7 @@
 # Initializer to configure resque daemon
-Resque.redis = ENV['REDIS_URL'] if ENV['REDIS_URL']
+if ENV['REDIS_URL']
+  Resque.redis = ENV['REDIS_URL']
+else
+  puts 'WARNING: redis is not installed, so Resque is using inline method.  (not recommended for production)'
+  Resque.inline = true
+end
