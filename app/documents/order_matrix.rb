2dddce101deef5cce0e1bc19ab0ba0a0eb3fed1c
Tom Carchrae <tom@intellecti.ca>
2019-03-31 22:49:01 -0700

    add back our custom split sheets

--- app/documents/order_matrix.rb
+++ app/documents/order_matrix.rb
@@ -1,117 +1,148 @@
+# encoding: utf-8
 class OrderMatrix < OrderPdf
 
-  HEADER_ROTATE = -30
-  PLACEHOLDER_CHAR = 'X'
+  MAX_ARTICLES_PER_PAGE = 8 # How many order_articles on each page
+
+
+  def initialize(order, options = {})
+    super(order, options)
+    @order = Order.find(order[0]) if order.is_a? Array
+  end
 
   def filename
     I18n.t('documents.order_matrix.filename', :name => @order.name, :date => @order.ends.to_date) + '.pdf'
   end
 
   def title
+    @order = Order.find(order[0]) if @order.is_a? Array
     I18n.t('documents.order_matrix.title', :name => @order.name,
-      :date => @order.ends.strftime(I18n.t('date.formats.default')))
+           :date => @order.ends.strftime(I18n.t('date.formats.default')),
+           :user_name => @order.created_by.name
+    )
   end
 
   def body
-    order_articles_data = [[
-      OrderArticle.human_attribute_name(:article),
-      Article.human_attribute_name(:supplier),
-      ArticlePrice.human_attribute_name(:unit_quantity),
-      OrderArticle.human_attribute_name(:units_received),
-      Article.human_attribute_name(:fc_price_short)
-    ]]
-
-    each_order_article do |a|
-      order_articles_data << [a.article.name,
-                              a.article.supplier.name,
-                              a.price.unit_quantity,
-                              a.units,
-                              order_article_price_per_unit(a)]
-    end
-
-    order_articles_data.each { |row| row.delete_at 1 } unless @options[:show_supplier]
-
-    name = I18n.t('documents.order_matrix.heading', count: order_articles_data.size-1)
-    nice_table name, order_articles_data do |table|
-      if @options[:show_supplier]
-        table.column(0).width = bounds.width / 3
-        table.column(1).width = bounds.width / 4
+    @orders.each_with_index do |order, _i|
+      if order.is_a? Fixnum
+        @order = Order.find(order)
       else
-        table.column(0).width = bounds.width / 2
+        @order = order
       end
 
-      table.columns(-3..-1).align = :right
-      table.column(-2).font_style = :bold
+      text @order.supplier.name + ' ordered by ' + @order.created_by.name
+      move_down 10
+
+      unless @order.note.blank?
+        text 'note: ' + @order.note, size: fontsize(9)
+        move_down 5
+      end
+
+      @order.comments.each_with_index do |comment, i|
+        if (i == 0)
+          text 'Comments', size: fontsize(9)
+          move_down 5
+        end
+        text comment.user.name + ' wrote: ' + comment.text, size: fontsize(9)
+        move_down 5
+      end
+
+      move_down 10
     end
 
-    font_size 8
 
-    row_height_1 = height_of(PLACEHOLDER_CHAR) + 3
-    col_width_0 = width_of(PLACEHOLDER_CHAR * 20)
-    col_width_1 = width_of("#{number_to_currency(888.88)} / #{PLACEHOLDER_CHAR * 4}") + 3
-    col_width_2 = width_of(PLACEHOLDER_CHAR * 3) + 5
+    @orders.each_with_index do |order, _i|
+      # start_new_page(layout: :landscape) if i > 0
 
-    first_page = true
-    start_new_page(layout: :landscape)
-    batch_size = (bounds.width - col_width_0 - col_width_1) / col_width_2
-    batch_size = batch_size.floor
-
-    each_ordergroup_batch batch_size do |batch_groups, batch_results|
-      start_new_page unless first_page
-
-      header = batch_groups.map do |name, total|
-        text = "#{name.try(:truncate, 20)} <b>#{number_to_currency(total)}</b>"
-        RotatedCell.new(self, text, inline_format: true, rotate: HEADER_ROTATE)
+      if order.is_a? Fixnum
+        @order = Order.find(order)
+      else
+        @order = order
       end
 
-      rows = [[nil, nil] + header]
 
-      last_supplier_id = -1
+      order_articles = @order.order_articles.ordered.sort_by {|o| o.article.name}
 
-      each_order_article do |order_article|
-        supplier = order_article.article.supplier
-        if @options[:show_supplier] && last_supplier_id != supplier.id
-          row = [make_cell(supplier.name, colspan: 2, font_style: :bold)]
-          batch_groups.each { row << nil }
-          rows << row
-          last_supplier_id = supplier.id
+      total_num_order_articles = order_articles.size
+      page_number = 0
+      while page_number * MAX_ARTICLES_PER_PAGE < total_num_order_articles do # Start page generating
+
+        page_number += 1
+        start_new_page(layout: :landscape)
+
+        # Collect order_articles for this page
+        current_order_articles = order_articles.select do |a|
+          order_articles.index(a) >= (page_number - 1) * MAX_ARTICLES_PER_PAGE and
+              order_articles.index(a) < page_number * MAX_ARTICLES_PER_PAGE
         end
 
-        row = [order_article.article.name, order_article_price_per_unit(order_article)]
-        row += batch_results[order_article.id] if batch_results[order_article.id]
-        rows << row
-      end
-
-      table rows, header: true, cell_style: {overflow: :shrink_to_fit} do |table|
-        table.cells.padding = [0, 0, 2, 0]
-        table.cells.borders = [:left]
-        table.cells.border_width = 0.5
-        table.cells.border_color = '666666'
-
-        table.row(0).borders = [:bottom, :left]
-        table.row(0).padding = [2, 0, 2, 0]
-        table.row(1..-1).height = row_height_1
-        table.column(0..1).borders = []
-        table.column(1).align = :right
-        table.column(1).padding = [0, 3, 2, 0]
-        table.column(2..-1).align = :center
-        table.cells[0,0].borders = []
-        table.cells[0,1].borders = []
-
-        table.column(0).overflow = :truncate
-        table.column(0).width = col_width_0
-        table.column(1).width = col_width_1
-        table.column(2..-1).width = col_width_2
-
-        (0..batch_size).step(5).each do |idx|
-          table.column(2+idx).border_width = 2
+        # Make order_articles header
+        header = [""]
+        for header_article in current_order_articles
+          name = header_article.article.name.gsub(/[-\/]/, " ").gsub(".", ". ").gsub(/\s+/, ' ')
+          name = name.split.collect {|w| w.truncate(8)}.join(" ")
+          header << name.truncate(30)
         end
 
-        table.row_colors = ['dddddd', 'ffffff']
-      end
+        # Collect group results
+        groups_data = [header]
 
-      first_page = false
+        @order.group_orders.includes(:ordergroup).sort_by(&:ordergroup_name).each do |group_order|
+
+          group_result = [group_order.ordergroup_name.truncate(20)]
+
+          for order_article in current_order_articles
+            # get the Ordergroup result for this order_article
+            goa = order_article.group_order_articles.where(group_order_id: group_order.id).first
+            result = ((goa.nil? || goa.result == 0) ? '' : goa.result.to_i)
+            group_result << result
+          end
+          groups_data << group_result
+        end
+
+        group_result = ['Total Units']
+        for order_article in current_order_articles
+          # get the Ordergroup result for this order_article
+          group_result << " #{order_article.units * order_article.price.unit_quantity} X #{order_article.article.unit}"
+        end
+        groups_data << group_result
+
+        group_result = ['Cases']
+        for order_article in current_order_articles
+          # get the Ordergroup result for this order_article
+          group_result << "#{order_article.units}"
+        end
+        groups_data << group_result
+
+        groups_data << ['% Spoilage'] + current_order_articles.map {|_a| ''}
+
+        # Make table
+        column_widths = [85]
+        (MAX_ARTICLES_PER_PAGE + 1).times {|i| column_widths << (656 / (MAX_ARTICLES_PER_PAGE + 1)).floor unless i == 0}
+        table groups_data, column_widths: column_widths, cell_style: {size: fontsize(8), overflow: :shrink_to_fit} do |table|
+          # table.row(0).style(:size => 6)
+          table.cells.border_width = 1
+          table.cells.border_color = '666666'
+          table.row_colors = ['ffffff', 'ececec']
+          table.row(groups_data.length - 1).style(bold: true)
+        end
+
+        move_down 10
+        text 'Always take a photo if there is any suspicion that produce is spoiled. Include the box/lot code so we can request refunds.'
+      end
     end
+  end
+
+  private
+
+  # Return price for article.
+  #
+  # This is a separate method so that plugins can override it.
+  #
+  # @param article [Article]
+  # @return [Number] Price to show
+  # @see https://github.com/foodcoops/foodsoft/issues/445
+  def article_price(article)
+    article.price.fc_price
   end
 
 end
