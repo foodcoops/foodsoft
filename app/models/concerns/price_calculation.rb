56d7f46aa21fd99274cf878772df6950d3e27461
Tom Carchrae <tom@intellecti.ca>
2019-04-03 16:09:36 -0700

    formula won't work with nil values

--- app/models/concerns/price_calculation.rb
+++ app/models/concerns/price_calculation.rb
@@ -12,14 +12,14 @@ module PriceCalculation
     add_percent(gross_price, FoodsoftConfig[:price_markup])
   end
 
+  def supplier_price
+    read_attribute(:supplier_price) || (unit_quantity * price unless unit_quantity.nil? || price.nil?)
+  end
+
   private
 
   def add_percent(value, percent)
     (value * (percent * 0.01 + 1)).round(2)
-  end
-
-  def supplier_price
-    read_attribute(:supplier_price) || unit_quantity * price
   end
 
 end
