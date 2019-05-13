6fc3fbbb7f300b33c3abbff1417f4d10b7acbde8
Julius <>
2019-05-13 14:49:28 +0200

    Minor improvements and cleanups for stock takings

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,8 +1,6 @@
 # encoding: utf-8
 class DeliveriesController < ApplicationController
 
-  before_filter :find_supplier, :exclude => :fill_new_stock_article_form
-  
   def index
     @deliveries = @supplier.deliveries.order('delivered_on DESC')
   end
@@ -18,7 +16,7 @@ class DeliveriesController < ApplicationController
 
   def create
     @delivery = Delivery.new(params[:delivery])
-    
+
     if @delivery.save
       flash[:notice] = I18n.t('deliveries.create.notice')
       redirect_to [@supplier, @delivery]
@@ -30,7 +28,7 @@ class DeliveriesController < ApplicationController
   def edit
     @delivery = Delivery.find(params[:id])
   end
-  
+
   def update
     @delivery = Delivery.find(params[:id])
 
@@ -49,22 +47,22 @@ class DeliveriesController < ApplicationController
     flash[:notice] = I18n.t('deliveries.destroy.notice')
     redirect_to supplier_deliveries_url(@supplier)
   end
-  
+
   def add_stock_change
     @stock_change = StockChange.new
     @stock_change.stock_article = StockArticle.find(params[:stock_article_id])
     render :layout => false
   end
-  
+
   def form_on_stock_article_create # See publish/subscribe design pattern in /doc.
     @stock_article = StockArticle.find(params[:id])
-    
+
     render :layout => false
   end
 
   def form_on_stock_article_update # See publish/subscribe design pattern in /doc.
     @stock_article = StockArticle.find(params[:id])
-    
+
     render :layout => false
   end
 
