8c8b42c2b2977a6549bf41c25856b5752c7e7d4e
wvengen <dev-voko@willem.engen.nl>
2020-07-25 15:57:53 +0200

    Fix schema issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181201000210) do
+ActiveRecord::Schema.define(version: 20181205000000) do
 
   create_table "article_categories", force: :cascade do |t|
     t.string "name",        limit: 255, default: "", null: false
