785313ac23e4c023ad1ec28de6e8dad3b815e098
Patrick Gansterer <paroga@paroga.com>
2020-08-01 20:11:25 +0200

    Make StockEvent a base class for Delivery and StockTaking
    This helps to share code between the two entities and allows easier
    extensions in the future.

---
+++ app/models/stock_event.rb
@@ -0,0 +1,8 @@
+class StockEvent < ApplicationRecord
+
+  has_many :stock_changes, dependent: :destroy
+  has_many :stock_articles, through: :stock_changes
+
+  validates_presence_of :date
+
+end
--- app/models/stock_taking.rb
+++ app/models/stock_taking.rb
@@ -1,10 +1,4 @@
-class StockTaking < ApplicationRecord
-
-  has_many :stock_changes, :dependent => :destroy
-  has_many :stock_articles, :through => :stock_changes
-
-  validates_presence_of :date
-
+class StockTaking < StockEvent
   def stock_change_attributes=(stock_change_attributes)
     for attributes in stock_change_attributes
       stock_changes.build(attributes) unless attributes[:quantity].to_i == 0
