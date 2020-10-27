9d9ef78ba3b7527e97df9556b7dd0d62a5da79e9
Tom Carchrae <tom@intellecti.ca>
2020-10-26 19:09:29 -0700

    handle duplicate order_numbers in spreadsheet import

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -8,15 +8,15 @@ class Supplier < ApplicationRecord
   has_many :orders
   has_many :deliveries
   has_many :invoices
-  belongs_to :shared_supplier  # for the sharedLists-App
+  belongs_to :shared_supplier # for the sharedLists-App
 
   include ActiveModel::MassAssignmentSecurity
   attr_accessible :name, :address, :phone, :phone2, :fax, :email, :url, :contact_person, :customer_number, :iban, :custom_fields,
                   :delivery_days, :order_howto, :note, :shared_supplier_id, :min_order_quantity, :shared_sync_method
 
-  validates :name, :presence => true, :length => { :in => 4..30 }
-  validates :phone, :presence => true, :length => { :in => 8..25 }
-  validates :address, :presence => true, :length => { :in => 8..50 }
+  validates :name, :presence => true, :length => {:in => 4..30}
+  validates :phone, :presence => true, :length => {:in => 8..25}
+  validates :address, :presence => true, :length => {:in => 8..50}
   validates_format_of :iban, :with => /\A[A-Z]{2}[0-9]{2}[0-9A-Z]{,30}\z/, :allow_blank => true
   validates_uniqueness_of :iban, :case_sensitive => false, :allow_blank => true
   validates_length_of :order_howto, :note, maximum: 250
@@ -61,7 +61,7 @@ class Supplier < ApplicationRecord
           .find_each { |new_shared_article| new_articles << new_shared_article.build_new_article(self) }
       # make them unavailable when desired
       if shared_sync_method == 'all_unavailable'
-        new_articles.each {|new_article| new_article.availability = false }
+        new_articles.each { |new_article| new_article.availability = false }
       end
     end
     return [updated_article_pairs, outlisted_articles, new_articles]
@@ -73,38 +73,66 @@ class Supplier < ApplicationRecord
   # @param options [Hash] Options passed to {FoodsoftFile#parse} except when listed here.
   # @option options [Boolean] :outlist_absent Set to +true+ to remove articles not in spreadsheet.
   # @option options [Boolean] :convert_units Omit or set to +true+ to keep current units, recomputing unit quantity and price.
-  def sync_from_file(file, options={})
+  def sync_from_file(file, options = {})
     all_order_numbers = []
     updated_article_pairs, outlisted_articles, new_articles = [], [], []
+    articles_by_order_number = articles.undeleted.group_by(&:order_number)
+    order_numbers_from_file = Set.new
     FoodsoftFile::parse file, options do |status, new_attrs, line|
-      article = articles.undeleted.where(order_number: new_attrs[:order_number]).first
-      new_attrs[:article_category] = ArticleCategory.find_match(new_attrs[:article_category])
-      new_attrs[:tax] ||= FoodsoftConfig[:tax_default]
-      new_article = articles.build(new_attrs)
-
-      if status.nil?
-        if article.nil?
-          new_articles << new_article
-        else
-          unequal_attributes = article.unequal_attributes(new_article, options.slice(:convert_units))
-          unless unequal_attributes.empty?
-            article.attributes = unequal_attributes
-            updated_article_pairs << [article, unequal_attributes]
+      # if there are duplicates in the file, we just take the first one
+      if (order_numbers_from_file.add?(new_attrs[:order_number]).nil?)
+        puts "skipping duplicate #{new_attrs[:order_number]}"
+      else
+        begin
+          # puts "matching #{new_attrs[:order_number]}"
+          articles_matching = articles_by_order_number[new_attrs[:order_number]] || []
+          articles_matching_in_open_orders = articles_matching.select { |a| a.in_open_order }
+          in_open_order = articles_matching_in_open_orders.count > 0
+          if (in_open_order)
+            article = articles_matching_in_open_orders.first
+          else
+            article = articles_matching.first
           end
-        end
-      elsif status == :outlisted && article.present?
-        outlisted_articles << article
 
-        # stop when there is a parsing error
-      elsif status.is_a? String
-        # @todo move I18n key to model
-        raise I18n.t('articles.model.error_parse', :msg => status, :line => line.to_s)
+          new_attrs[:article_category] = ArticleCategory.find_match(new_attrs[:article_category])
+          new_attrs[:tax] ||= FoodsoftConfig[:tax_default]
+          new_article = articles.build(new_attrs)
+
+          if status.nil?
+            if article.nil?
+              new_articles << new_article
+            else
+              unequal_attributes = article.unequal_attributes(new_article, options.slice(:convert_units))
+              unless unequal_attributes.empty?
+                article.attributes = unequal_attributes
+                updated_article_pairs << [article, unequal_attributes]
+              end
+            end
+          elsif status == :outlisted && article.present?
+            outlisted_articles << article
+
+            # stop when there is a parsing error
+          elsif status.is_a? String
+            # @todo move I18n key to model
+            raise I18n.t('articles.model.error_parse', :msg => status, :line => line.to_s)
+          end
+
+          duplicate_articles = articles_matching.select { |a| !(a == article || a.in_open_order) }
+          if (duplicate_articles.count > 0)
+            puts "found #{duplicate_articles.count} extra copies for #{new_attrs[:order_number]} #{in_open_order ? 'in open order' : ''} " +
+                     " #{new_attrs[:name]} (new #{new_articles.size} updated  #{updated_article_pairs.size})"
+            outlisted_articles.concat(duplicate_articles)
+          end
+
+          all_order_numbers << article.order_number if article
+        rescue RuntimeError => e
+          puts "blew up #{e.inspect}"
+        end
       end
 
-      all_order_numbers << article.order_number if article
     end
     if options[:outlist_absent]
-      outlisted_articles += articles.undeleted.where.not(order_number: all_order_numbers+[nil])
+      outlisted_articles += articles.undeleted.where.not(order_number: all_order_numbers + [nil])
     end
     return [updated_article_pairs, outlisted_articles, new_articles]
   end
@@ -134,8 +162,8 @@ class Supplier < ApplicationRecord
   def sum_on_pickup_date(date, sum_type)
     @sum_on_pickup_date ||= {}
     @sum_on_pickup_date[[date, sum_type]] ||= begin
-      orders.where(pickup: date).map { |o| o.sum(sum_type) }.sum
-    end
+                                                orders.where(pickup: date).map { |o| o.sum(sum_type) }.sum
+                                              end
   end
 
   def notify_open_orders_updated
