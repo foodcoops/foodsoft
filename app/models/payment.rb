1077858dc2d02a4686da41982710d6661fa8ea12
Christoph Echtinger-Sieghart <sigi@0x2a.at>
2020-12-29 21:50:14 +0100

    Cleanup payment model

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,8 +4,6 @@ class Payment < ApplicationRecord
   #include FindEachWithOrder
   include MarkAsDeletedWithName
 
-  #has_many :memberships, dependent: :destroy
-  #has_many :users, -> { where(deleted_at: nil) }, through: :memberships
   has_and_belongs_to_many :groups
 
   validates :name, :presence => true, :length => {:in => 1..25}
@@ -15,16 +13,6 @@ class Payment < ApplicationRecord
 
   scope :undeleted, -> { where(deleted_at: nil) }
 
-  ## Returns true if the given user if is an member of this group.
-  #def member?(user)
-  #  memberships.find_by_user_id(user.id)
-  #end
-
-  ## Returns all NONmembers and a checks for possible multiple Ordergroup-Memberships
-  #def non_members
-  #  User.natural_order.reject { |u| users.include?(u) }
-  #end
-  #
   def group_tokens=(ids)
     # Make sure only references to Ordergroups are allowed. This also has the
     # nice side effect, that ordergroups can only be added once
@@ -33,15 +21,6 @@ class Payment < ApplicationRecord
 
   def deleted?
     deleted_at.present?
-  end
-
-  def mark_as_deleted
-    # TODO: Checks for participating in not closed orders
-    transaction do
-      #memberships.destroy_all
-      # @todo what should happen to the users?
-      super
-    end
   end
 
   # Make sure, the name is uniqe and add a useful message if there already exists a deleted payment
