a96dbd563b52ea2848add34f98fb1d892f9bd2b7
Florian Lentsch <office@florian-lentsch.at>
2021-02-05 11:42:50 +0100

    Migrations for new `received` order state (s. #779)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_05_010000) do
+ActiveRecord::Schema.define(version: 2021_02_05_090257) do
 
   create_table "article_categories", force: :cascade do |t|
     t.string "name", default: "", null: false
