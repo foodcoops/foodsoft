ddc870745ac15d3cb59c16083e77c0dd5ac059f1
Patrick Gansterer <paroga@paroga.com>
2021-02-18 20:05:17 +0100

    Add API v1 finance_overview endpoint

---
+++ app/controllers/api/v1/user/ordergroup_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::User::OrdergroupController < Api::V1::BaseController
+
+  before_action ->{ doorkeeper_authorize! 'finance:user' }, only: [:financial_overview]
+
+  def financial_overview
+    ordergroup = Ordergroup.include_transaction_class_sum.find(current_ordergroup.id)
+
+    render json: {
+      financial_overview: {
+        account_balance: ordergroup.account_balance.to_f,
+        available_funds: ordergroup.get_available_funds.to_f,
+        financial_transaction_class_sums: FinancialTransactionClass.sorted.map { |c|
+          {
+            id: c.id,
+            name: c.display,
+            amount: ordergroup["sum_of_class_#{c.id}"].to_f
+          }
+        }
+      }
+    }
+  end
+
+end
