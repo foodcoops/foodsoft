f42845051635df913fe04d62779840f53172ef67
Viehlieb <3feuerba@informatik.uni-hamburg.de>
2021-12-24 13:31:10 +0100

    left over todo for naming od pdf file

--- app/controllers/concerns/send_group_order_invoice_pdf.rb
+++ app/controllers/concerns/send_group_order_invoice_pdf.rb
@@ -3,9 +3,9 @@ module Concerns::SendGroupOrderInvoicePdf
 
   protected
 
-  def send_group_order_invoice_pdf group_order_invoice
+  def send_group_order_invoice_pdf(group_order_invoice)
     invoice_data = group_order_invoice.load_data_for_invoice
-    invoice_data[:title] = "Rechnung für " + invoice_data[:supplier] # TODO Internationalise und load_data rausschmeißen
+    invoice_data[:title] = t('documents.group_order_invoice_pdf.title', supplier: invoice_data[:supplier])
     pdf = GroupOrderInvoicePdf.new group_order_invoice.load_data_for_invoice
     send_data pdf.to_pdf, filename: pdf.filename, type: 'application/pdf'
   end
