c59715381e38de19785f0912c91f8b03e0549195
Patrick Gansterer <paroga@paroga.com>
2022-02-20 03:30:27 +0100

    Configure base_controller of Doorkeeper

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -2,6 +2,8 @@ Doorkeeper.configure do
   # Change the ORM that doorkeeper will use (needs plugins)
   orm :active_record
 
+  base_controller 'ApplicationController'
+
   # This block will be called to check whether the resource owner is authenticated or not.
   resource_owner_authenticator do
     authenticate
@@ -110,11 +112,4 @@ Doorkeeper.configure do
 
   # WWW-Authenticate Realm (default "Doorkeeper").
   realm 'Foodsoft'
-end
-
-# my take on https://github.com/doorkeeper-gem/doorkeeper/issues/465
-ActiveSupport.on_load(:after_initialize) do
-  Doorkeeper::ApplicationController.send :include, Concerns::Locale
-  Doorkeeper::ApplicationController.send :include, Concerns::FoodcoopScope
-  Doorkeeper::ApplicationController.send :include, Concerns::Auth
 end
