7a4013c96b199c0c30d93c4ef2584d8d28d4db6d
haraldreingruber <harald.reingruber@gmx.at>
2022-03-07 22:53:05 +0000

    Implement PR Feedback

--- spec/models/ordergroup_spec.rb
+++ spec/models/ordergroup_spec.rb
@@ -1,20 +1,4 @@
-require './db/seeds/seed_helper'
 require_relative '../spec_helper'
-
-# should be extracted to a helper file
-def create_order(starts)
-  og = user.ordergroup
-
-  SupplierCategory.create!(:id => 1, :name => "Other", :financial_transaction_class_id => ftc1.id)
-  Supplier.create!([{ :id => 1, :name => "Beautiful bakery", :supplier_category_id => 1, :address => "Smallstreet 1, Cookilage", :phone => "0123456789", :email => "info@bbakery.test", :min_order_quantity => "100" }])
-  ArticleCategory.create!(:id => 5, :name => "Bread & Bakery")
-  Article.create!(:name => "Brown whole", :supplier_id => 1, :article_category_id => 5, :unit => "pc", :note => "organic", :availability => true, :manufacturer => "The Baker", :origin => "NL", :price => 0.22E1, :tax => 6.0, :deposit => 0.0, :unit_quantity => 1)
-
-  order = seed_order(supplier_id: 1, starts: starts, ends: starts + 5.days, created_by_user_id: user.id, updated_by_user_id: user.id)
-
-  go = og.group_orders.create!(order: order, updated_by_user_id: 1)
-  go.group_order_articles.find_or_create_by!(order_article: order.order_articles.first)
-end
 
 describe Ordergroup do
   let(:ftc1) { create :financial_transaction_class }
@@ -25,12 +9,16 @@ describe Ordergroup do
   let(:user) { create :user, groups: [create(:ordergroup)] }
 
   it 'shows no active ordergroups when all orders are older than 3 months' do
-    create_order(4.months.ago)
+    order = create :order, starts: 4.months.ago
+    user.ordergroup.group_orders.create!(order: order)
+
     expect(Ordergroup.active).to be_empty
   end
 
   it 'shows active ordergroups when there are recent orders' do
-    create_order(2.days.ago)
+    order = create :order, starts: 2.days.ago
+    user.ordergroup.group_orders.create!(order: order)
+
     expect(Ordergroup.active).not_to be_empty
   end
 
