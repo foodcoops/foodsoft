45ae1928914e89fa6886110b5026b7e8b2d80f27
viehlieb <pf@pragma-shift.net>
2023-02-14 12:25:41 +0100

    move BigDecimal.new to BigDecimal()

--- config/initializers/extensions.rb
+++ config/initializers/extensions.rb
@@ -3,7 +3,7 @@ class String
   # remove comma from decimal inputs
   def self.delocalized_decimal(string)
     if !string.blank? and string.is_a?(String)
-      BigDecimal.new(string.sub(',', '.'))
+      BigDecimal(string.sub(',', '.'))
     else
       string
     end
