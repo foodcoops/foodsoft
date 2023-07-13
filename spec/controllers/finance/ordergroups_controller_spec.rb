a2b9c0069afe97463c5b616026505336acf6e347
Philipp Rothmann <philipprothmann@posteo.de>
2023-07-13 16:03:45 +0200

    change tests to use assert_select

--- spec/controllers/finance/ordergroups_controller_spec.rb
+++ spec/controllers/finance/ordergroups_controller_spec.rb
@@ -3,24 +3,30 @@
 require 'spec_helper'
 
 describe Finance::OrdergroupsController do
+  include ActionView::Helpers::NumberHelper
+  render_views
+
   let(:user) { create(:user, :role_finance, :role_orders, :ordergroup) }
   let(:fin_trans_type1) { create(:financial_transaction_type) }
   let(:fin_trans_type2) { create(:financial_transaction_type) }
   let(:fin_trans1) do
     create(:financial_transaction,
            user: user,
+           amount: 100,
            ordergroup: user.ordergroup,
            financial_transaction_type: fin_trans_type1)
   end
   let(:fin_trans2) do
     create(:financial_transaction,
            user: user,
+           amount: 200,
            ordergroup: user.ordergroup,
            financial_transaction_type: fin_trans_type1)
   end
   let(:fin_trans3) do
     create(:financial_transaction,
            user: user,
+           amount: 42.23,
            ordergroup: user.ordergroup,
            financial_transaction_type: fin_trans_type2)
   end
@@ -37,14 +43,15 @@ describe Finance::OrdergroupsController do
     it 'renders index page' do
       get_with_defaults :index
       expect(response).to have_http_status(:success)
-      expect(response).to render_template('finance/ordergroups/index')
     end
 
     it 'calculates total balance sums correctly' do
       get_with_defaults :index
-      expect(assigns(:total_balances).size).to eq(2)
-      expect(assigns(:total_balances)[fin_trans_type1.id]).to eq(fin_trans1.amount + fin_trans2.amount)
-      expect(assigns(:total_balances)[fin_trans_type2.id]).to eq(fin_trans3.amount)
+      expect(response).to have_http_status(:success)
+
+      assert_select "#total_balance#{fin_trans_type1.id}", number_to_currency(300)
+      assert_select "#total_balance#{fin_trans_type2.id}", number_to_currency(42.23)
+      assert_select '#total_balance_sum', number_to_currency(342.23)
     end
   end
 end
