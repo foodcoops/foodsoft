ad05870669af3fc634209ac4c540ab6883174a11
twothreenine <leonard_ostler@yahoo.de>
2024-04-20 06:15:39 +0200

    Improve financial link feature & transaction links
    - add a config option "use_financial_links" for activating both finance links and foodcoop transaction features
    - add migration: set this option to true if any financial link or foodcoop transaction exists
    - add options to create a financial link and/or foodcoop transaction when balancing an order
    - show links to finance links only to users with finance role (required anyway, but links were shown in home/ordergroup)
    - add note column when adding a financial transaction to a link
    - financial transactions: show link to group_order only in home/ordergroup and (additionally) dashboard; replace with link to order's balancing page in finance views
    - finance link: show link in financial transaction note to respective order's balancing page (also when adding a financial transaction)
    - finance link: replace misleading "delete" button for removing a transaction with "remove from link", also adapt confirm message
    - adapt en/de locales, fix finance-related locale typos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_26_111615) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_04_004950) do
   create_table "action_text_rich_texts", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "name", null: false
     t.text "body", size: :long
