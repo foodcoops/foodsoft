d13b50a978c0af08702f6f37ac835a7d2c2bbae6
Philipp Rothmann <philipprothmann@posteo.de>
2024-07-05 00:40:37 +0200

    add up/down to migration

--- db/migrate/20240410093313_change_bank_account_desc.rb
+++ db/migrate/20240410093313_change_bank_account_desc.rb
@@ -1,5 +1,9 @@
 class ChangeBankAccountDesc < ActiveRecord::Migration[7.0]
-  def change
+  def up
     change_column :bank_accounts, :description, :text
+  end
+
+  def down
+    change_column :bank_accounts, :description, :string
   end
 end
