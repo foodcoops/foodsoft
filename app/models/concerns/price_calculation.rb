ee949bb0affe0f9a96b10574c3163a32dbc31f4d
mjavurek <57668028+mjavurek@users.noreply.github.com>
2024-12-17 12:38:30 +0100

    Update price_calculation.rb: no markup on deposit (optionally?)

--- app/models/concerns/price_calculation.rb
+++ app/models/concerns/price_calculation.rb
@@ -3,22 +3,30 @@ module PriceCalculation
 
   # Gross price = net price + deposit + tax.
   # @return [Number] Gross price.
-  def gross_price
-    add_percent(price + deposit, tax)
+  def gross_price(include_deposit=TRUE)
+    if include_depost
+      add_percent(price + deposit, tax)
+    else
+      add_percent(price, tax)
+    end
   end
 
   # @return [Number] Price for the foodcoop-member.
   def fc_price
-    add_percent(gross_price, FoodsoftConfig[:price_markup].to_i)
+      add_percent(gross_price(include_deposit=FALSE), FoodsoftConfig[:price_markup]) + fc_deposit
   end
 
-  # deposit
   def gross_deposit
     add_percent(deposit, tax)
   end
 
   def fc_deposit
-    add_percent(gross_deposit, FoodsoftConfig[:price_markup])
+    deposit_with_markup = FALSE # FoodsoftConfig[:deposit_with_markup]
+    if deposit_with_markup  
+      add_percent(gross_deposit, FoodsoftConfig[:price_markup])
+    else
+      gross_deposit
+    end
   end
 
   private
