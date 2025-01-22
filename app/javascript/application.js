f232a804762ad64557825f1ab8e166c52085d3e3
Henning Schumann <henning.schumann@unicode-it.de>
2025-01-22 13:02:41 +0100

    chore(deps): update to ruby 3.3

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require jquery
+//= require bootstrap-sprockets
 import "trix"
 import "@rails/actiontext"
 import "trix-editor-overrides"
