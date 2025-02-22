b4a8c78db09fe7183bce5f86ed62485c2f806404
Martin Ortbauer <mortbauer@gmail.com>
2025-02-22 15:13:22 +0100

    check if query is present before using it in the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   # Currently used to display users nick and ids for autocomplete
   def index
-    @users = User.undeleted.natural_search(params[:q])
+    @users = User.undeleted.natural_search(params[:q]) if params[:q].present?
     respond_to do |format|
       format.json { render json: @users.map(&:token_attributes).to_json }
     end
