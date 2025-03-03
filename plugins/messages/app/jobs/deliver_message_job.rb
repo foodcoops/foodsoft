55390a1db91ec250b4c8d8004b857b8bb3dbf815
Martin Ortbauer <mortbauer@gmail.com>
2025-03-03 19:23:33 +0100

    catch exception in deliver_now loop for each recipient fix #12

--- plugins/messages/app/jobs/deliver_message_job.rb
+++ plugins/messages/app/jobs/deliver_message_job.rb
@@ -1,15 +1,29 @@
 class DeliverMessageJob < ApplicationJob
   def perform(message)
+    @errors = Array.new
     message.message_recipients.each do |message_recipient|
+      # Skip the action if email_state is :sent
+      next if message_recipient.email_state == 'sent'
+
       recipient = message_recipient.user
       if recipient.receive_email?
-        Mailer.deliver_now_with_user_locale recipient do
-          MessagesMailer.foodsoft_message(recipient, message)
+        begin
+          Mailer.deliver_now_with_user_locale recipient do
+            MessagesMailer.foodsoft_message(recipient, message)
+          end
+          message_recipient.update_attribute :email_state, :sent
+        rescue => e
+          # Handle the exception (e.g., log the error, set email_state to :failed, etc.)
+          # Rails.logger.error (["Failed to deliver message #{message.id} to recipient #{recipient.id}:"]+e.backtrace).join("\n")
+          @errors << e
+          message_recipient.update_attribute :email_state, :failed
         end
-        message_recipient.update_attribute :email_state, :sent
       else
         message_recipient.update_attribute :email_state, :skipped
       end
+    end
+    unless @errors.empty?
+      raise StandardError.new (["At least deliery to one recipient for message: #{message.id} failed"]+@errors).join("\n")
     end
   end
 end
--- plugins/messages/app/models/message_recipient.rb
+++ plugins/messages/app/models/message_recipient.rb
@@ -2,5 +2,5 @@ class MessageRecipient < ActiveRecord::Base
   belongs_to :message
   belongs_to :user
 
-  enum email_state: %i[pending sent skipped]
+  enum email_state: %i[pending sent skipped failed]
 end
