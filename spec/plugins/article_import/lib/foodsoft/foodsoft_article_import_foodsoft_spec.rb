52ef059a0c53cf11f40a4aea0a3752102d20569e
Robert <rw@roko.li>
2025-04-17 15:50:43 +0200

    style: fix spec linting errors
    Refs #1100

--- spec/plugins/article_import/lib/foodsoft/foodsoft_article_import_foodsoft_spec.rb
+++ spec/plugins/article_import/lib/foodsoft/foodsoft_article_import_foodsoft_spec.rb
@@ -12,69 +12,72 @@ describe FoodsoftArticleImport do
                     article_unit_ratios: [], billing_unit: nil, group_order_granularity: nil, group_order_unit: nil,
                     minimum_order_quantity: nil, price_unit: nil, supplier_order_unit: nil }
 
-  dummy_article_2 = { availability: false, order_number: '12', name: 'other product', note: 'bio', manufacturer: 'someone', # rubocop:todo Naming/VariableNumber
-                      origin: 'eu', unit: '2 kg', price: '3.45', tax: '6', article_category: 'coolstuff', deposit: '0',
-                      article_unit_ratios: [], billing_unit: nil, group_order_granularity: nil, group_order_unit: nil,
-                      minimum_order_quantity: nil, price_unit: nil, supplier_order_unit: nil }
+  dummy_article2 = { availability: false, order_number: '12', name: 'other product', note: 'bio', manufacturer: 'someone',
+                     origin: 'eu', unit: '2 kg', price: '3.45', tax: '6', article_category: 'coolstuff', deposit: '0',
+                     article_unit_ratios: [], billing_unit: nil, group_order_granularity: nil, group_order_unit: nil,
+                     minimum_order_quantity: nil, price_unit: nil, supplier_order_unit: nil }
 
-  articles = [dummy_article, dummy_article_2]
+  articles = [dummy_article, dummy_article2]
 
-  dummy_article_3 = dummy_article.merge({ order_number: ':c72fb13' })
-  dummy_article_4 = dummy_article_2.merge({ order_number: ':cd9ffa6' })
-  articles_number_generated = [dummy_article_3, dummy_article_4]
-  empty = {}
+  dummy_article3 = dummy_article.merge({ order_number: ':c72fb13' })
+  dummy_article4 = dummy_article2.merge({ order_number: ':cd9ffa6' })
+  articles_number_generated = [dummy_article3, dummy_article4]
 
-  context 'foodsoft' do
+  context 'with type foodsoft' do
     it 'parses foodsoft file correctly without type parameter' do
       count = 0
-      FoodsoftArticleImport.parse(File.open(File.join(foodsoft_files_path,
-                                                      'foodsoft_flawless.csv'))) do |new_attrs, status, _line|
+      described_class.parse(File.open(File.join(foodsoft_files_path,
+                                                'foodsoft_flawless.csv'))) do |new_attrs, status, _line|
         expect(new_attrs).to eq articles[count]
-        expect(status).to eq nil
+        expect(status).to be_nil
         count += 1
       end
     end
 
     it 'parses file correctly with type parameter foodsoft' do
       count = 0
-      FoodsoftArticleImport.parse(File.open(File.join(foodsoft_files_path, 'foodsoft_flawless.csv')),
-                                  type: 'foodsoft') do |new_attrs, status, _line|
+      described_class.parse(File.open(File.join(foodsoft_files_path, 'foodsoft_flawless.csv')),
+                            type: 'foodsoft') do |new_attrs, status, _line|
         expect(new_attrs).to eq articles[count]
-        expect(status).to eq nil
+        expect(status).to be_nil
         count += 1
       end
     end
+
     it 'raises error wenn wrong type specified' do
       expect do
-        FoodsoftArticleImport.parse(File.open(File.join(foodsoft_files_path, 'foodsoft_flawless.csv')),
-                                    type: 'bioromeo')
+        described_class.parse(File.open(File.join(foodsoft_files_path, 'foodsoft_flawless.csv')),
+                              type: 'bioromeo')
       end.to raise_error(Roo::HeaderRowNotFoundError)
-      expect(FoodsoftArticleImport.parse(File.open(File.join(foodsoft_files_path, 'foodsoft_flawless.csv')),
-                                         type: 'odin')).to eq []
+      expect(described_class.parse(File.open(File.join(foodsoft_files_path, 'foodsoft_flawless.csv')),
+                                   type: 'odin')).to eq []
 
-      expect(FoodsoftArticleImport.parse(File.open(File.join(foodsoft_files_path, 'foodsoft_flawless.csv')),
-                                         type: 'bnn')).to eq []
+      expect(described_class.parse(File.open(File.join(foodsoft_files_path, 'foodsoft_flawless.csv')),
+                                   type: 'bnn')).to eq []
     end
+
     it 'parses missing entries correctly' do
-      FoodsoftArticleImport.parse(File.open(File.join(foodsoft_files_path, 'foodsoft_missing_entries.csv')),
-                                  type: 'foodsoft') do |new_attrs, status, _line|
-        expect(status).to eq nil
+      described_class.parse(File.open(File.join(foodsoft_files_path, 'foodsoft_missing_entries.csv')),
+                            type: 'foodsoft') do |new_attrs, status, _line|
+        expect(status).to be_nil
         expect(new_attrs[:unit]).to eq '1 kg'
-        expect(new_attrs[:manufacturer]).to eq nil
+        expect(new_attrs[:manufacturer]).to be_nil
       end
     end
+
     it 'generates order numbers for articles without order number' do
       count = 0
-      FoodsoftArticleImport.parse(File.open(File.join(foodsoft_files_path, 'foodsoft_generate_order_number.csv')),
-                                  type: 'foodsoft') do |new_attrs, _status, _line|
+      described_class.parse(File.open(File.join(foodsoft_files_path, 'foodsoft_generate_order_number.csv')),
+                            type: 'foodsoft') do |new_attrs, _status, _line|
         expect(new_attrs).to eq articles_number_generated[count]
         count += 1
       end
     end
-    xit 'joins custom_codes file' do
+
+    it 'joins custom_codes file', skip: 'fails with "No such file or directory"' do
       custom_file_path = File.join(files_path, 'custom_codes.yml').to_s
-      FoodsoftArticleImport.parse(File.open(File.join(foodsoft_files_path, 'foodsoft_flawless_custom_category.csv')),
-                                  custom_file_path: custom_file_path, type: 'foodsoft') do |new_attrs, _status, _line|
+      described_class.parse(File.open(File.join(foodsoft_files_path, 'foodsoft_flawless_custom_category.csv')),
+                            custom_file_path: custom_file_path, type: 'foodsoft') do |new_attrs, _status, _line|
         expect(new_attrs[:article_category]).to eq 'Test Indeling - Test Subindeling'
       end
     end
