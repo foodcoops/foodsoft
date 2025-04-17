7a29b5d033815a09884ddcf5da0557034e79c33e
Robert <rw@roko.li>
2025-04-17 15:17:15 +0200

    test: fix import test
    Refs #1100

--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -8,11 +8,11 @@ describe Supplier do
       article1 = create(:article, supplier: supplier, order_number: 177_813, unit: '250 g', price: 0.1)
       article2 = create(:article, supplier: supplier, order_number: 12_345)
       supplier.articles = [article1, article2]
-      options = { filename: 'foodsoft_file_01.csv' }
+      options = {}
       options[:outlist_absent] = true
       options[:convert_units] = true
       updated_article_pairs, outlisted_articles, new_articles = supplier.sync_from_file(
-        Rails.root.join('spec/fixtures/foodsoft_file_01.csv'), options
+        File.open(Rails.root.join('spec/fixtures/foodsoft_file_01.csv')), options
       )
       expect(new_articles.length).to be > 0
       expect(updated_article_pairs.first[1][:name]).to eq 'Tomaten'
