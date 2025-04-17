23bfe3806a98e1b5899f5c2d6f8e066f90a0109e
Robert <rw@roko.li>
2025-04-17 15:06:54 +0200

    test: include article import specs into foodsoft
    Refs #1100

--- spec/plugins/article_import/lib/odin/foodsoft_article_import_odin_spec.rb
+++ spec/plugins/article_import/lib/odin/foodsoft_article_import_odin_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-require_relative '../../../lib/foodsoft_article_import'
+require 'foodsoft_article_import'
 
 describe FoodsoftArticleImport do
   files_path = File.expand_path '../../files', __dir__
