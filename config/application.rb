7dd670f931cc5aeef63f3bfc85c9e547b2755f5d
Henning Schumann <henning.schumann@unicode-it.de>
2025-05-23 12:24:39 +0200

    chore: set default settings to rails 7.2

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Foodsoft
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
