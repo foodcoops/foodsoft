b3f70877c30d0aead821c08873c7d17ba505db8f
Robert <rw@roko.li>
2025-07-08 12:11:01 +0200

    feat(invoices): add basic data model

---
+++ plugins/invoices/app/helpers/invoices_helper.rb
@@ -0,0 +1,17 @@
+module InvoicesHelper
+  SEPA_SEQUENCE_TYPES = {
+    FRST: I18n.t('helpers.invoices.sepa_frst'),
+    RCUR: I18n.t('helpers.invoices.sepa_rcur'),
+    OOFF: I18n.t('helpers.invoices.sepa_ooff'),
+    FNAL: I18n.t('helpers.invoices.sepa_fnal')
+  }.freeze
+
+  def generate_invoice_number(instance, count)
+    trailing_number = count.to_s.rjust(4, '0')
+    if GroupOrderInvoice.find_by(invoice_number: instance.invoice_date.strftime("%Y%m%d") + trailing_number)
+      generate_invoice_number(instance, count.to_i + 1)
+    else
+      instance.invoice_date.strftime("%Y%m%d") + trailing_number
+    end
+  end
+end
