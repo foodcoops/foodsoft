3b54bdd6b802b808a3efbb00b4d6a9e7059b2d6e
Robert <rw@roko.li>
2025-07-24 12:47:06 +0200

    feat(invoices): add bulk creation for group order invoices
    Introduces route, view, and controller support for bulk creation.
    Provides form and modal updates for improved UI interaction.

--- plugins/invoices/app/controllers/group_order_invoices_controller.rb
+++ plugins/invoices/app/controllers/group_order_invoices_controller.rb
@@ -1,4 +1,5 @@
 class GroupOrderInvoicesController < OrderInvoicesControllerBase
+  include InvoicesHelper
   include SendGroupOrderInvoicePdf
 
   def show
--- plugins/invoices/config/routes.rb
+++ plugins/invoices/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   scope '/:foodcoop' do
+    post 'finance/group_order_invoice', to: 'group_order_invoices#create_multiple'
+
     resources :group_order_invoices do
       member do
         patch :toggle_paid
