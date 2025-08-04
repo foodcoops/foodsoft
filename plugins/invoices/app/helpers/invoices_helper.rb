5c50ec0fae9db5f23d3ca537a06bdfa8768fd869
Robert <rw@roko.li>
2025-08-04 13:17:15 +0200

    feat(invoices): add OrdergroupInvoicesController with full CRUD and batch actions
    Introduces OrdergroupInvoicesController for handling group invoice operations.
    Includes batch actions such as sending, toggling statuses, and downloading.
    Updates routes and views to support new functionality.

--- plugins/invoices/app/helpers/invoices_helper.rb
+++ plugins/invoices/app/helpers/invoices_helper.rb
@@ -1,7 +1,7 @@
 module InvoicesHelper
   def generate_invoice_number(instance, count)
     trailing_number = count.to_s.rjust(4, '0')
-    if GroupOrderInvoice.find_by(invoice_number: instance.invoice_date.strftime('%Y%m%d') + trailing_number)
+    if GroupOrderInvoice.find_by(invoice_number: instance.invoice_date.strftime('%Y%m%d') + trailing_number) || OrdergroupInvoice.find_by(invoice_number: instance.invoice_date.strftime('%Y%m%d') + trailing_number)
       generate_invoice_number(instance, count.to_i + 1)
     else
       instance.invoice_date.strftime('%Y%m%d') + trailing_number
