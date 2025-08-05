3ccf316ac351b17261c31fcf6b7781522a4aa726
Robert <rw@roko.li>
2025-08-05 13:10:14 +0200

    feat(invoices): automate group order invoice email notifications
    Adds a subscription to `foodsoft.order.closed` event to trigger
    automatic invoice creation and email notifications. Implements the
    `NotifyGroupOrderInvoiceJob` for sending emails and updates locales with
     relevant translations. Includes sample configuration for enabling the
     feature.

--- plugins/invoices/lib/foodsoft_invoices/engine.rb
+++ plugins/invoices/lib/foodsoft_invoices/engine.rb
@@ -7,6 +7,17 @@ module FoodsoftInvoices
         Finance::BalancingController.include BalancingControllerExtensions
         GroupOrder.include GroupOrderExtensions
         Order.include OrderExtensions
+
+        # Send group order invoices when order is closed
+        ActiveSupport::Notifications.subscribe('foodsoft.order.closed') do |*args|
+          order = ActiveSupport::Notifications::Event.new(*args).payload[:order]
+          if FoodsoftConfig[:group_order_invoices]&.[](:use_automatic_invoices) && order.closed?
+            order.group_orders.each do |go|
+              goi = GroupOrderInvoice.find_or_create_by!(group_order_id: go.id)
+              NotifyGroupOrderInvoiceJob.perform_later(goi) if goi.save!
+            end
+          end
+        end
       end
     end
 
