b48e1bb2bc00f9b9e9fd0718dbeed34b35bc16b1
Robert <rw@roko.li>
2025-08-09 14:44:39 +0200

    test(invoices): add model specs and factories

--- plugins/invoices/spec/models/group_order_invoice_spec.rb
+++ plugins/invoices/spec/models/group_order_invoice_spec.rb
@@ -8,8 +8,10 @@ describe GroupOrderInvoice do
   let(:group_order) { create(:group_order, order: order, ordergroup: user.ordergroup) }
 
   describe 'erroneous group order invoice' do
+    let(:invoice) { create(:group_order_invoice, group_order_id: group_order.id) }
+
     it 'does not create group order invoice if tax_number not set' do
-      expect { described_class.create!(group_order_id: group_order.id) }.to raise_error(ActiveRecord::RecordInvalid, /.*/)
+      expect { invoice }.to raise_error(ActiveRecord::RecordInvalid, /.*/)
     end
   end
 
@@ -22,35 +24,37 @@ describe GroupOrderInvoice do
     invoice_number2 = Time.now.strftime('%Y%m%d') + '0002'
 
     let(:user2) { create(:user, groups: [create(:ordergroup)]) }
-    let(:group_order2) { create(:group_order, order: order, ordergroup: user2.ordergroup) }
+
+    let(:invoice) { create(:group_order_invoice, group_order_id: group_order.id) }
+    let(:invoice_duplicate) { create(:group_order_invoice, group_order_id: group_order.id) }
+
+    let(:another_order_group_order) { create(:group_order, order: order, ordergroup: user2.ordergroup) }
+
+    let(:another_invoice) { create(:group_order_invoice, group_order_id: another_order_group_order.id) }
+    let(:invoice_with_duplicate_number) { create(:group_order_invoice, group_order_id: another_order_group_order.id, invoice_number: invoice_number1) }
 
     it 'creates group order invoice if tax_number is set' do
-      goi = described_class.new(group_order_id: group_order.id)
-      expect(goi).to be_valid
+      expect(invoice).to be_valid
     end
 
     it 'sets invoice_number according to date' do
-      goi = described_class.create!(group_order_id: group_order.id)
       number = Time.now.strftime('%Y%m%d') + '0001'
-      expect(goi.invoice_number).to eq(number.to_i)
+      expect(invoice.invoice_number).to eq(number.to_i)
     end
 
     it 'fails to create if group_order_id is used multiple times for creation' do
-      goi1 = described_class.create!(group_order_id: group_order.id)
-      expect(goi1.group_order.id).to eq(group_order.id)
-      expect { described_class.create!(group_order_id: group_order.id) }.to raise_error(ActiveRecord::RecordInvalid)
+      expect(invoice.group_order.id).to eq(group_order.id)
+      expect { invoice_duplicate }.to raise_error(ActiveRecord::RecordInvalid)
     end
 
     it 'creates two different group order invoice with different invoice_numbers' do
-      goi1 = described_class.create!(group_order_id: group_order.id)
-      goi3 = described_class.create!(group_order_id: group_order2.id)
-      expect(goi1.invoice_number).to eq(invoice_number1.to_i)
-      expect(goi3.invoice_number).to eq(invoice_number2.to_i)
+      expect(invoice.invoice_number).to eq(invoice_number1.to_i)
+      expect(another_invoice.invoice_number).to eq(invoice_number2.to_i)
     end
 
     it 'fails to create two different group order invoice with same invoice_numbers' do
-      described_class.create!(group_order_id: group_order.id)
-      expect { described_class.create!(group_order_id: group_order2.id, invoice_number: invoice_number1.to_i) }.to raise_error(ActiveRecord::RecordInvalid)
+      invoice
+      expect { invoice_with_duplicate_number }.to raise_error(ActiveRecord::RecordInvalid)
     end
   end
 end
---
+++ plugins/invoices/spec/models/multi_group_order_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe MultiGroupOrder do
+  let(:admin) { create(:user, groups: [create(:workgroup, role_finance: true), create(:ordergroup, name: 'AdminOrders')]) }
+  let(:user) { create(:user, groups: [create(:ordergroup)]) }
+
+  context 'when orders are not closed' do
+    it 'is not generated without valid multi_order' do
+      order1 = create(:order)
+      order2 = create(:order)
+      create(:group_order, ordergroup: user.ordergroup, order: order1)
+      create(:group_order, ordergroup: user.ordergroup, order: order2)
+      expect { create(:multi_order, orders: [order1, order2]) }.to raise_error(ActiveRecord::RecordInvalid)
+      expect(described_class.count).to eq(0)
+    end
+  end
+
+  context 'when orders are closed' do
+    it 'is created by MultiOrder' do
+      order1 = create(:order)
+      order2 = create(:order)
+      create(:group_order, ordergroup: user.ordergroup, order: order1)
+      create(:group_order, ordergroup: user.ordergroup, order: order2)
+      order1.update!(state: 'closed')
+      order2.update!(state: 'closed')
+      create(:multi_order, orders: [order1, order2])
+      expect(described_class.count).to eq(1)
+    end
+  end
+end
