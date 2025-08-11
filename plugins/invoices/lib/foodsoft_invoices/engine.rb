6ac7b10cf6bf9b7ac180e09c4d3498aaf08453c2
Robert <rw@roko.li>
2025-08-11 11:32:51 +0200

    refactor(invoices): modularize extension enabling logic
    Moves extension enabling logic to a separate method for reusability.
    Updates specs to ensure extensions are explicitly enabled before tests.
    This fixes javascript bug.

--- plugins/invoices/lib/foodsoft_invoices/engine.rb
+++ plugins/invoices/lib/foodsoft_invoices/engine.rb
@@ -1,33 +1,35 @@
 module FoodsoftInvoices
   class Engine < ::Rails::Engine
     config.to_prepare do
-      if FoodsoftInvoices.enabled?
-        # Register assets
-        Foodsoft::AssetRegistry.register_stylesheet('foodsoft_invoices')
-        Foodsoft::AssetRegistry.register_javascript('foodsoft_invoices')
-        # Register controller extensions
-        Finance::BalancingController.include BalancingControllerExtensions
-        OrdersController.include OrdersControllerExtensions
-        # Register model extensions
-        Group.include GroupExtensions
-        GroupOrder.include GroupOrderExtensions
-        Order.include OrderExtensions
-
-        # Send group order invoices when order is closed
-        ActiveSupport::Notifications.subscribe('foodsoft.order.closed') do |*args|
-          order = ActiveSupport::Notifications::Event.new(*args).payload[:order]
-          if FoodsoftConfig[:group_order_invoices]&.[](:use_automatic_invoices) && order.closed?
-            order.group_orders.each do |go|
-              goi = GroupOrderInvoice.find_or_create_by!(group_order_id: go.id)
-              NotifyGroupOrderInvoiceJob.perform_later(goi) if goi.save!
-            end
-          end
-        end
-      end
+      enable_extensions! if FoodsoftInvoices.enabled?
     end
 
     def default_foodsoft_config(cfg)
       cfg[:use_invoices] = false
+    end
+  end
+
+  def self.enable_extensions!
+    # Register assets
+    Foodsoft::AssetRegistry.register_stylesheet('foodsoft_invoices')
+    Foodsoft::AssetRegistry.register_javascript('foodsoft_invoices')
+    # Register controller extensions
+    Finance::BalancingController.include BalancingControllerExtensions
+    OrdersController.include OrdersControllerExtensions
+    # Register model extensions
+    Group.include GroupExtensions
+    GroupOrder.include GroupOrderExtensions
+    Order.include OrderExtensions
+
+    # Send group order invoices when order is closed
+    ActiveSupport::Notifications.subscribe('foodsoft.order.closed') do |*args|
+      order = ActiveSupport::Notifications::Event.new(*args).payload[:order]
+      if FoodsoftConfig[:group_order_invoices]&.[](:use_automatic_invoices) && order.closed?
+        order.group_orders.each do |go|
+          goi = GroupOrderInvoice.find_or_create_by!(group_order_id: go.id)
+          NotifyGroupOrderInvoiceJob.perform_later(goi) if goi.save!
+        end
+      end
     end
   end
 end
