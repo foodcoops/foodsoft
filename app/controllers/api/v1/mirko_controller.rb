6c88be2575e21f422d45fb90d9db1a7d0d638342
mjavurek <57668028+mjavurek@users.noreply.github.com>
2025-12-17 10:13:21 +0100

    Update mirko_controller.rb: include open stock orders (#15)

--- app/controllers/api/v1/mirko_controller.rb
+++ app/controllers/api/v1/mirko_controller.rb
@@ -3,26 +3,34 @@ class Api::V1::MirkoController < Api::V1::BaseController
 
   def index
     time_now = Time.now
-    gos = GroupOrder
-          .includes(group_order_articles: { order_article: :article },
-                    order: :supplier)
-          .references(:orders)
-          .where(ordergroup_id: current_user.ordergroup.id)
-          .limit(200)
-          .where(orders: {
-                   ends: (time_now.weeks_ago(1)..time_now.next_year), state: "open", pickup: nil
-                 })
-          .or(
-            GroupOrder
-              .includes(group_order_articles: { order_article: :article },
-                        order: :supplier)
-              .references(:orders)
-              .where(ordergroup_id: current_user.ordergroup.id)
-              .limit(200)
-              .where(orders: {
-                       pickup: ((time_now.weeks_ago(5)..(time_now.weeks_ago(-1)))), state: %w[finished received closed]
-                     })
-          )
+    base_scope = GroupOrder
+      .includes(group_order_articles: { order_article: :article },
+                order: :supplier)
+      .references(:orders)
+      .where(ordergroup_id: current_user.ordergroup.id)
+      .limit(200)
+    gos = base_scope.where(
+      orders: {
+        state: "open",
+        pickup: nil, # z.B. Leergut Rückgabe
+        ends: time_now.weeks_ago(1)..time_now.next_year
+      }
+    ).or(
+      base_scope.where(
+        orders: {
+          state: "open",
+          supplier_id: nil, # Lager
+          ends: time_now.weeks_ago(1)..time_now.next_year
+        }
+      )  
+    ).or(
+      base_scope.where(
+        orders: {
+          state: %w[finished received closed],
+          pickup: time_now.weeks_ago(5)..time_now.weeks_ago(-1)
+        }
+      )
+    )
     render json: gos, each_serializer: MirkoSerializer
   end
 end
