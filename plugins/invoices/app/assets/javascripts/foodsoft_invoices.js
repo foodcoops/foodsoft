4ba2c335dc68821e301fe3c9ea301d7fd813881a
Henning Schumann <henning.schumann@unicode-it.de>
2026-09-18 16:41:36 +0200

    fix: funcy behaviour of sepa type select field, caused by unneeded link

--- plugins/invoices/app/assets/javascripts/foodsoft_invoices.js
+++ plugins/invoices/app/assets/javascripts/foodsoft_invoices.js
@@ -39,22 +39,6 @@ $(document).on('click', '.merge-orders-btn', function () {
     });
 });
 
-$(document).off('change', '[class^="ajax-update-all-link-"] select').on('change', '[class^="ajax-update-all-link-"] select', function () {
-    var selectedValue = $(this).val();
-    var url = $(this).closest('a').attr('href');
-    $.ajax({
-        url: url,
-        method: 'PATCH',
-        data: { sepa_sequence_type: selectedValue },
-        success: function (response) {
-            // Handle success response
-        },
-        error: function (error) {
-            console.log(error);
-        }
-    });
-});
-
 $(document).off('change', '.ajax-update-sepa-select').on('change', '.ajax-update-sepa-select', function () {
     var selectedValue = $(this).val();
     var url = $(this).data('url');
